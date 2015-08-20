baef0883f174d0c1ba3a5e56551da4cd691b3f25
Marky Mark <maxhope@users.noreply.github.com>
2015-08-20 15:34:12 +0200

    first commit

---
+++ Help.py
@@ -0,0 +1,14 @@
+class Help:
+    def help_message(self):
+        help_string = '-pgphome\t\tThe pgp home folder containing the public and private keyrings\n' \
+                      '-type\t\t\tThe target type. Must be either "file" or "folder". E.g: [-type file /tmp/myfile.txt] or [-type folder /tmp/myfolder]\n' \
+                      '-output\t\t\tThe output file\n' \
+                      '-encrypt\t\tThe file to be encrypted\n' \
+                      '-decrypt\t\tThe file to be decrypted\n' \
+                      '\n\n\n' \
+                      'Examples:\n' \
+                      'Encrypt a file: ./pygpg -pgphome ~/.gnupg -encrypt -type file /tmp/myfile.txt -output /tmp/myfile.txt.pgp\n' \
+                      'Encrypt a folder: ./pygpg -pgphome ~/.gnupg -encrypt -type folder /tmp/myfolder -output /tmp/myfolder.pgp\n' \
+                      'Decrypt a file or folder: ./pygpg -pgphome ~/.gnupg -decrypt /tmp/myfile.txt.pgp -output /tmp/myfile.txt\n' \
+
+        return help_string
---
+++ pygpg.py
@@ -0,0 +1,144 @@
+import os
+import gnupg
+import sys
+from os.path import expanduser
+from Help import Help
+import tarfile
+
+def parse_arguments():
+    noOfArguments = len(sys.argv)
+
+    home_is_set = False
+    enc = False
+    dec = False
+
+    if noOfArguments == 1:
+        print_help()
+    else:
+        for arg in range(0, noOfArguments):
+            if sys.argv[arg] == '-pgphome':
+                home = sys.argv[arg + 1]
+                set_gnupgp_home(home)
+                home_is_set = True
+
+            if sys.argv[arg] == '-encrypt':
+                typearg = sys.argv[arg + 1]
+                if typearg != '-type':
+                    print "Expecting -type argument! E.g -encrypt -type folder <foldername>"
+                else:
+                    type = sys.argv[arg + 2]
+                    target = sys.argv[arg + 3]
+                    if len(sys.argv) > arg + 4:
+                        if sys.argv[arg + 4]  == '-output':
+                            output = sys.argv[arg + 5]
+                        else:
+                            raise Exception("Expecting output file!")
+                    else:
+                        output = None
+                    enc = True
+
+            if sys.argv[arg] == '-decrypt':
+                target = sys.argv[arg + 1]
+                if len(sys.argv) > arg + 2:
+                    if sys.argv[arg + 2]  == '-output':
+                        output = sys.argv[arg + 3]
+                    else:
+                        raise Exception("Expecting output file!")
+                else:
+                    output = None
+                dec = True
+
+
+    if not home_is_set:
+        home = None
+        set_gnupgp_home(home)
+
+    if enc and dec:
+        raise Exception("You cannot both encrypt and decrypt a file at the same time!")
+    elif enc:
+        encrypt(type, target, output)
+    elif dec:
+        decrypt(target, output)
+
+def decrypt(target, output):
+    print "Decrypting..."
+
+    encrypted_data = open(target, "rb")
+
+    if output != None:
+        gpg.decrypt_file(encrypted_data, output=output)
+        print "Decrypted data is saved to",output
+    else:
+        print "\nThe decrypted message:\n"
+        decrypted_data = gpg.decrypt_file(encrypted_data)
+        print decrypted_data
+
+def encrypt(type, target, output):
+    if type == 'folder':
+        print "Encrypting folder..."
+        tar = tarfile.open(target+'/contents.tar.gz', "w:gz")
+        tar.add(target, arcname=os.path.basename(target))
+        tar.close()
+
+        print_public_keys()
+        uid = input("\nSelect recipient: ")
+        recipient = get_recipient_fingerprint(uid)
+
+        data = open(target+'/contents.tar.gz', "rb")
+
+        if output != None:
+            gpg.encrypt_file(data, [recipient], output=output)
+            print "Encrypted data is saved to",output
+        else:
+            print "\nThe encrypted message:\n"
+            encrypted_ascii_data = gpg.encrypt_file(data, [recipient])
+            print encrypted_ascii_data
+
+        os.remove(target+'/contents.tar.gz')
+
+    elif type == 'file':
+        print "Encrypting file..."
+        print_public_keys()
+        uid = input("\nSelect recipient: ")
+        recipient = get_recipient_fingerprint(uid)
+
+        data = open(target, "rb")
+
+        if output != None:
+            gpg.encrypt_file(data, [recipient], output=output)
+            print "Encrypted data is saved to",output
+        else:
+            print "\nThe encrypted message:\n"
+            encrypted_ascii_data = gpg.encrypt_file(data, [recipient])
+            print encrypted_ascii_data
+    else:
+        raise Exception("Type must be 'folder' or 'file'!")
+
+def set_gnupgp_home(home):
+    if home is None:
+        home = expanduser("~")+'/.gnupg'
+
+    global gpg
+    gpg = gnupg.GPG(gnupghome=home)
+
+def get_recipient_fingerprint(uid):
+    public_keys = gpg.list_keys()
+    return public_keys[uid]['fingerprint']
+
+def main():
+    #print_help()
+    parse_arguments()
+
+
+def print_help():
+    h = Help()
+    print h.help_message()
+
+def print_public_keys():
+    public_keys = gpg.list_keys()
+    print "\nRecipients:"
+    for pub in range(0, len(public_keys)):
+        print str(pub)+':', public_keys[pub]['uids'], public_keys[pub]['fingerprint']
+
+if __name__ == "__main__":
+    main()
